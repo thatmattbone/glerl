e7eb88fa87ea1b782dedec93ce582a00f0c2f3cf
Matt Bone <thatmattbone@gmail.com>
2023-10-24 22:54:29 -0500

    the realtime app runs

---
+++ glerl_realtime/lib/glerl/supervisor.ex
@@ -0,0 +1,23 @@
+defmodule Glerl.Realtime.Supervisor do
+  use Supervisor
+
+  require Logger
+
+  def start_link(init_arg) do
+    Logger.info("Glerl.Supervisor.start_link/ #{inspect(init_arg)}")
+
+    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  @impl true
+  def init(init_arg) do
+    Logger.info("Glerl.Supervisor.init/1, #{inspect(init_arg)}")
+
+    children = [
+      # {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one},
+      Glerl.Realtime.Poller
+    ]
+
+    Supervisor.init(children, strategy: :one_for_all)
+  end
+end
