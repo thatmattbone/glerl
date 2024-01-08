b8eaf17fb8d41572dc352ab9d8a3dc1a4826d905
Matt Bone <thatmattbone@gmail.com>
2024-01-08 00:42:10 -0600

    fix the stubbed out tests

---
+++ test/glerl/realtime/poller_test.exs
@@ -0,0 +1,8 @@
+defmodule Glerl.Realtime.PollerTest do
+  use ExUnit.Case
+  doctest Glerl.Realtime.Poller
+
+  #test "greets the world" do
+  #  assert Glerl.Realtime.hello() == :world
+  #end
+end
--- test/glerl/realtime/realtime_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Glerl.RealtimeTest do
-  use ExUnit.Case
-  # doctest Glerl.Realtime
-
-  test "greets the world" do
-    assert Glerl.Realtime.hello() == :world
-  end
-end
