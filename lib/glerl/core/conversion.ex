abf809703b889b7c97bb997ab697f2ce10132e74
Matt Bone <thatmattbone@gmail.com>
2024-03-11 23:09:00 -0500

    some formatting

--- lib/glerl/core/conversion.ex
+++ lib/glerl/core/conversion.ex
@@ -7,11 +7,11 @@ defmodule Glerl.Core.Conversion do
 
   # convert degrees to 16 point compass rose directions.
   @spec degrees_to_direction(number()) :: String.t()
-  def degrees_to_direction(degrees) when degrees >= 0 and degrees < 11.25, do: "N"
-  def degrees_to_direction(degrees) when degrees >= 11.25 and degrees < 33.75, do: "NNE"
-  def degrees_to_direction(degrees) when degrees >= 33.75 and degrees < 56.25, do: "NE"
-  def degrees_to_direction(degrees) when degrees >= 56.25 and degrees < 78.75, do: "ENE"
-  def degrees_to_direction(degrees) when degrees >= 78.75 and degrees < 101.25, do: "E"
+  def degrees_to_direction(degrees) when degrees >= 0      and degrees <  11.25, do: "N"
+  def degrees_to_direction(degrees) when degrees >= 11.25  and degrees <  33.75, do: "NNE"
+  def degrees_to_direction(degrees) when degrees >= 33.75  and degrees <  56.25, do: "NE"
+  def degrees_to_direction(degrees) when degrees >= 56.25  and degrees <  78.75, do: "ENE"
+  def degrees_to_direction(degrees) when degrees >= 78.75  and degrees < 101.25, do: "E"
   def degrees_to_direction(degrees) when degrees >= 101.25 and degrees < 123.75, do: "ESE"
   def degrees_to_direction(degrees) when degrees >= 123.75 and degrees < 146.25, do: "SE"
   def degrees_to_direction(degrees) when degrees >= 146.25 and degrees < 168.75, do: "SSE"
@@ -23,5 +23,5 @@ defmodule Glerl.Core.Conversion do
   def degrees_to_direction(degrees) when degrees >= 281.25 and degrees < 303.75, do: "WNW"
   def degrees_to_direction(degrees) when degrees >= 303.75 and degrees < 326.25, do: "NW"
   def degrees_to_direction(degrees) when degrees >= 326.25 and degrees < 348.75, do: "NNW"
-  def degrees_to_direction(degrees) when degrees >= 348.75 and degrees <= 360, do: "N"
+  def degrees_to_direction(degrees) when degrees >= 348.75 and degrees <= 360,   do: "N"
 end
