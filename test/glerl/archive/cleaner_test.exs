0164395fa7ffa5aeee82ae4b886a0cfd9f7abd65
Matt Bone <thatmattbone@gmail.com>
2024-09-09 09:42:44 -0500

    more test from yesterday

--- test/glerl/archive/cleaner_test.exs
+++ test/glerl/archive/cleaner_test.exs
@@ -12,7 +12,7 @@ defmodule Glerl.Archive.CleanerTest do
     assert Cleaner.fix_data([], start_time, end_time) == []
   end
 
-  test "fix list with small gap" do
+  test "fix two element list with small gap" do
     start_time = DateTime.from_naive!(~N[2011-05-11T10:00:00], "America/Chicago")
     end_time = DateTime.from_naive!(~N[2011-05-11T10:06:00], "America/Chicago")
 
@@ -53,5 +53,55 @@ defmodule Glerl.Archive.CleanerTest do
 
     assert c.timestamp == DateTime.from_naive!(~N[2011-05-11T10:04:00], "America/Chicago")
     assert %{b | timestamp: nil} == %{c | timestamp: nil}
+  end
+
+  test "fix four element list with three small gaps" do
+    start_time = DateTime.from_naive!(~N[2011-05-11T10:00:00], "America/Chicago")
+    end_time = DateTime.from_naive!(~N[2011-05-11T10:30:00], "America/Chicago")
+
+    datapoints = [
+      %Datapoint{
+        station_id: 4,
+        timestamp: start_time,
+        temp_c: 20.0,
+        speed: 10.4,
+        gusts: 11.5,
+        direction: 96,
+        humidity: 55.5
+      },
+      %Datapoint{
+        station_id: 4,
+        timestamp: DateTime.from_naive!(~N[2011-05-11T10:14:00], "America/Chicago"),
+        temp_c: 21.0,
+        speed: 11.4,
+        gusts: 12.1,
+        direction: 98,
+        humidity: 52.5
+      },
+      %Datapoint{
+        station_id: 4,
+        timestamp: DateTime.from_naive!(~N[2011-05-11T10:18:00], "America/Chicago"),
+        temp_c: 20.5,
+        speed: 10.4,
+        gusts: 11.5,
+        direction: 94,
+        humidity: 49.5
+      },
+      %Datapoint{
+        station_id: 4,
+        timestamp: end_time,
+        temp_c: 20.5,
+        speed: 9.4,
+        gusts: 11.1,
+        direction: 97,
+        humidity: 48.2
+      }
+    ]
+
+    Cleaner.check_data(datapoints)
+
+    fixed = Cleaner.fix_data(datapoints)
+
+    assert length(fixed) == 16
   end
 end
